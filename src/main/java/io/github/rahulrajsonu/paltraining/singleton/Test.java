86ded4b03f49545d13173d3ce1c7688d30740b9b
RAHUL RAJ <RAJRAHUL9939@GMAIL.COM>
2020-07-03 15:40:10 +0530

    Simple singleton class breaking by reflection

--- src/main/java/io/github/rahulrajsonu/paltraining/singleton/Test.java
+++ src/main/java/io/github/rahulrajsonu/paltraining/singleton/Test.java
@@ -1,11 +1,20 @@
 package io.github.rahulrajsonu.paltraining.singleton;
 
+import java.lang.reflect.Constructor;
+
 public class Test {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception{
     Singleton s1 = Singleton.getInstance();
-    Singleton s2 = Singleton.getInstance();
+    Singleton s2 = breakSingleton();
     System.out.println(String.format("Object : %s, Hashcode: %d","s1",s1.hashCode()));
     System.out.println(String.format("Object : %s, Hashcode: %d","s2",s2.hashCode()));
+  }
+
+  static Singleton breakSingleton() throws Exception {
+    Class clazz = Class.forName("io.github.rahulrajsonu.paltraining.singleton.Singleton");
+    Constructor<Singleton> constructor = clazz.getDeclaredConstructor();
+    constructor.setAccessible(true);
+    return constructor.newInstance();
   }
 }

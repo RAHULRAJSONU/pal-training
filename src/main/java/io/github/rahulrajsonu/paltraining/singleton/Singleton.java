ad98038761a0cd6f53e381335e0bc85fc6ac7cde
RAHUL RAJ <RAJRAHUL9939@GMAIL.COM>
2020-07-03 22:36:19 +0530

    Double check locking

--- src/main/java/io/github/rahulrajsonu/paltraining/singleton/Singleton.java
+++ src/main/java/io/github/rahulrajsonu/paltraining/singleton/Singleton.java
@@ -3,17 +3,29 @@ package io.github.rahulrajsonu.paltraining.singleton;
 import java.io.Serializable;
 
 public class Singleton implements Serializable, Cloneable {
-  private final static Singleton singleton = new Singleton();
+  private static volatile Singleton singleton = null;
 
   private Singleton(){
+    if(null != singleton){
+      throw new RuntimeException("Operation not supported, Please use getInstance()");
+    }
     System.out.println("Creating new instance..");
   }
   public static Singleton getInstance(){
+    if(null==singleton){
+      synchronized (Singleton.class){
+        if(null==singleton){
+          singleton = new Singleton();
+        }
+      }
+    }
     return singleton;
   }
 
+
+
   @Override
   protected Object clone() throws CloneNotSupportedException {
-    return super.clone();
+    throw new RuntimeException("Cloning not supported.");
   }
 }
--- src/main/java/io/github/rahulrajsonu/paltraining/singleton/Test.java
+++ src/main/java/io/github/rahulrajsonu/paltraining/singleton/Test.java
@@ -5,7 +5,6 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Constructor;
-import java.util.Arrays;
 
 public class Test {
 
